15d440ecb3161934e7fbf09451fa58a8520b6d9f
Max Lapan <max.lapan@gmail.com>
2009-09-10 09:15:31 +0400

    Test case for printf

--- 03-full-console/kernel.c
+++ 03-full-console/kernel.c
@@ -11,6 +11,7 @@ char stack[8192];
 extern shared_info_t shared_info;
 shared_info_t *HYPERVISOR_shared_info;
 
+char buf[256];
 
 /* Main kernel entry point, called by trampoline */
 void start_kernel (start_info_t *start_info)
@@ -18,7 +19,7 @@ void start_kernel (start_info_t *start_info)
 	sched_shutdown_t op;
 
 	/* Map the shared info page */
-	HYPERVISOR_update_va_mapping((unsigned long) &shared_info, 
+	HYPERVISOR_update_va_mapping((unsigned long) &shared_info,
 			__pte(start_info->shared_info | 7),
 			UVMF_INVLPG);
 
@@ -27,6 +28,9 @@ void start_kernel (start_info_t *start_info)
 	init_events ();
 	console_init (start_info);
 	console_write ("Just a hello!\r\n");
+	
+	snprintf (buf, sizeof (buf), "1: %d, 2: %x\r\n", 1234, 1234);
+	console_write (buf);
 
 /* 	op.reason = SHUTDOWN_poweroff; */
 /* 	HYPERVISOR_sched_op (SCHEDOP_shutdown, &op); */
